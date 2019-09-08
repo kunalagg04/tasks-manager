595bc5a55fd2deb887dda35005abae6bfe340a6a
Kunal Aggarwal <kunalagg04@gmail.com>
2019-09-09 02:37:42 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,26 @@
-const auth = (req,res,next) => {
-     console.log('hi i am middleware')
-     next()
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+
+const auth = async (req,res,next) => {
+    
+     try{
+          const token = req.header('Authorization').replace('Bearer ','')
+          console.log(token)
+          const decoded = jwt.verify(token , 'hibc')
+          const user = await User.findOne({ _id : decoded._id , 'tokens.token' : token })
+          if (!user){
+               throw new Error()
+          }
+          req.user = user
+         
+          next()
+     }
+
+     catch(e){
+        res.status(401).send('please authenticate')
+     }
+    
 }
 
 module.exports = auth
